0fef385bf5b99549ad781549b167d6311229de84
liucheng <1678859776@qq.com>
2019-01-18 18:28:54 +0800

    增加.gitignore文件

--- src/main/java/crud/test/MapperTest.java
+++ src/main/java/crud/test/MapperTest.java
@@ -1,7 +1,6 @@
 package crud.test;
 
 
-import bean.Department;
 import bean.Employee;
 import dao.DepartmentMapper;
 import dao.EmployeeMapper;
@@ -16,7 +15,8 @@ import java.util.UUID;
 
 /**
  * 测试dao层的工作
- * @author  Xx、
+ *
+ * @author Xx、
  * 推荐spring的项目就可以用spring的单元测试，可以自动注入我们需要的组件
  * 1.导入SpringTest模块
  * 2.@ContextConfiguration指定Spring配置文件的位置
@@ -25,7 +25,7 @@ import java.util.UUID;
 
 //使用单元测试模块时，需要删除pm依赖中对单元测试的限制<scope>
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class MapperTest {
     @Autowired
     DepartmentMapper departmentMapper;
@@ -40,7 +40,7 @@ public class MapperTest {
      * 测试DepartmentMapper
      */
     @Test
-    public void testCRUD(){
+    public void testCRUD() {
         /*//1.创建SpringIOC容器
         ApplicationContext ioc= new ClassPathXmlApplicationContext("applicationContext.xml");
         //2.从容器中获取Mapper
@@ -56,16 +56,12 @@ public class MapperTest {
         //employeeMapper.insertSelective(new Employee(null,"TOM","N","TOM@ecut.com",1));
 
         //3.批量插入多个员工；使用可执行批量操作的sqlSession
-        EmployeeMapper mapper=sqlSession.getMapper(EmployeeMapper.class);
-        for(int i=0;i<10;i++){
-            String uid=UUID.randomUUID().toString().substring(0,5)+i;
-            mapper.insertSelective(new Employee(null,uid,"M",uid+"@ecut.com",1));
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+        for (int i = 0; i < 10; i++) {
+            String uid = UUID.randomUUID().toString().substring(0, 5) + i;
+            mapper.insertSelective(new Employee(null, uid, "M", uid + "@ecut.com", 1));
         }
         System.out.println("批量完成！");
-
-
-
-
 
 
     }
